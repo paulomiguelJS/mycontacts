1a4db09df4dd382308d3a38a66dd2a21845f3d95
paulomigueljs <paulomiguelblues@gmail.com>
2022-12-12 21:26:25 -0500

    Eslinst and edigotc onfig installed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,9 @@
-const { response } = require("express");
-const express = require("express");
+const express = require('express');
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.send("Hello World");
+app.get('/', (request, response) => {
+  response.send('Hello World');
 });
 
-app.listen(3000, () =>
-  console.log("Server has been started at http://localhost:3000")
-);
+app.listen(3000, () => console.log('Server has been started at http://localhost:3000'));
