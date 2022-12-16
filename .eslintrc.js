b6fdba3ebff728bdff3b3520b9485d0a77c33291
paulomigueljs <paulomiguelblues@gmail.com>
2022-12-15 22:30:00 -0500

    Show method created

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,5 +12,6 @@ module.exports = {
   rules: {
     'class-methods-use-this': 'off',
     'linebreak-style': 'off',
+    'consistent-return': 'off',
   },
 };
--- src/app/respositories/ContactsRepository.js
+++ src/app/respositories/ContactsRepository.js
@@ -1,12 +1,19 @@
-const { uuid } = require('uuidv4');
+const { v4 } = require('uuid');
 
 const contacts = [
   {
-    id: uuid(),
+    id: v4(),
     name: 'Paulo',
     email: 'paulo@email.com',
     phone: '123123123',
-    category_id: uuid(),
+    category_id: v4(),
+  },
+  {
+    id: v4(),
+    name: 'Carlos',
+    email: 'carlos@email.com',
+    phone: '55555',
+    category_id: v4(),
   },
 ];
 
@@ -15,6 +22,10 @@ class ContactsRepository {
     return new Promise((resolve) => {
       resolve(contacts);
     });
+  }
+
+  findById(id) {
+    return new Promise((resolve) => resolve(contacts.find((contact) => contact.id === id)));
   }
 }
 
