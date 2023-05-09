b9b1748d279e4016b8eab916b48f66ddc13a9803
paulomigueljs <paulomiguelblues@gmail.com>
2023-05-08 21:02:25 -0400

    Allowing Methods and Headers / Pre Flighted

--- src/app/middlewares/cors.js
+++ src/app/middlewares/cors.js
@@ -1,3 +1,5 @@
 module.exports = (request, response, next) => {
   response.setHeader('Access-Control-Allow-Origin', '*');
+  response.setHeader('Access-Control-Allow-Methods', '*');
+  response.setHeader('Access-Control-Allow-Headers', '*');
 };
